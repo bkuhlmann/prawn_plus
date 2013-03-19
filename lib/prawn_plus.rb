89319d21f111c1dc5e7a18a0e6d54d7cda8a7f28
Brooke Kuhlmann <brooke@redalchemist.com>
2013-03-18 21:36:53 -0600

    Cleaned up requirement path syntax.

--- lib/prawn_plus.rb
+++ lib/prawn_plus.rb
@@ -1,4 +1,4 @@
 require "prawn"
-require File.join File.dirname(__FILE__), "prawn_plus", "version.rb"
-require File.join File.dirname(__FILE__), "prawn_plus", "template_handlers", "prawn.rb"
-require File.join File.dirname(__FILE__), "prawn_plus", "railtie.rb"
+require "prawn_plus/version"
+require "prawn_plus/template_handlers/prawn"
+require "prawn_plus/railtie"
