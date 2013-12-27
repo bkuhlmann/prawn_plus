e503a61c3cc3599d74ca9fcba2e7080801dd4b21
Brooke Kuhlmann <brooke@redalchemist.com>
2013-12-27 15:22:34 -0700

    Fixed RSpec deprecation warnings for treating metadata symbol keys as true values. [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,6 @@ ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 # Configure Rspec.
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run focus: true
 
   Capybara.app = Dummy::Application
