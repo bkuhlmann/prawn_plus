55628367b5e07f5c0a23cf2edbac8d9181c777e7
Brooke Kuhlmann <brooke@redalchemist.com>
2014-09-07 09:49:14 -0600

    Refactored common RSpec configurations to RSpec support/kit folder.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,23 +33,8 @@ require "capybara/rails"
 # Load SQLite database in memory.
 ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 
-# Configure Rspec.
+Dir[File.join(File.dirname(__FILE__), "support/kit/**/*.rb")].each { |file| require file }
+
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run focus: true
-  config.order = "random"
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.expect_with :rspec do |expectations|
-    expectations.syntax = :expect
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.before(:all) { GC.disable }
-  config.after(:all) { GC.enable }
-
   Capybara.app = Dummy::Application
 end
