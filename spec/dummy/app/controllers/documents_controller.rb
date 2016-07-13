e5d0a20f7b10aaa8e954addbb45306b3a7cea5bd
Brooke Kuhlmann <brooke@alchemists.io>
2016-07-12 20:55:48 -0600

    Removed legacy dummy application.

--- spec/dummy/app/controllers/documents_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class DocumentsController < ApplicationController
-  def index
-  end
-end
--- spec/dummy/config/routes.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Dummy::Application.routes.draw do
-  resources :documents
-end
