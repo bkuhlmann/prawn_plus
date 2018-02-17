34cd5c62a79e64108a0e4fe8cab89bdc744dbb49
Brooke Kuhlmann <brooke@alchemists.io>
2018-02-17 14:29:14 -0700

    Fixed Rubocop Style/MissingElse issues.

--- spec/support/dummy/config/puma.rb
+++ spec/support/dummy/config/puma.rb
@@ -5,21 +5,11 @@ workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
 thread_count = Integer(ENV.fetch("MAX_THREADS", 5))
 threads thread_count, thread_count
 
-local_port = ENV.fetch "PORT", 3000
-local_root = ENV["HOME"]
-
 preload_app!
 
 rackup DefaultRackup
-port local_port
+port ENV.fetch("PORT", 3000)
 environment ENV.fetch("RACK_ENV", "development")
-
-if ENV["HTTPS_LOCAL"] == "true"
-  key = "#{local_root}/.ssl/localhost.key"
-  cert = "#{local_root}/.ssl/localhost.crt"
-
-  bind "ssl://localhost:#{local_port}?key=#{key}&cert=#{cert}"
-end
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection
