cae8ddb70bd32563eeda5a0af8575440bd2b19be
Brooke Kuhlmann <brooke@alchemists.io>
2021-02-13 11:37:19 -0700

    Added Ruby garbage collection compaction
    Necessary to leverage Ruby 3.0.0 automatic garbage collection
    compaction. The first line enables automatic compaction while the
    second line ensures the current application is compatible with
    automatic compaction.
    
    You'll want to look for crash ouput similar to `ruby -e
    "Process.kill(:SEGV, $$)"` and unexpected TypeErrors. Further details
    can be found [here](https://alanwu.space/post/check-compaction).

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,9 @@ ENV["RAILS_ROOT"] ||= File.dirname(__FILE__) + "/dummy"
 require "spec_helper"
 require "rspec/rails"
 
+GC.auto_compact = true
+GC.verify_compaction_references double_heap: true, toward: :empty
+
 RSpec.configure do |config|
   config.include AbstractController::Translation
   config.include ActiveSupport::Testing::TimeHelpers
