c68b7dbdf52d6030002937f81807adb89db8f291
Brooke Kuhlmann <brooke@alchemists.io>
2021-04-29 20:37:32 -0600

    Removed unicode capybara DSL
    Unnecessary since this is a request spec at the moment.

--- spec/requests/unicode_spec.rb
+++ spec/requests/unicode_spec.rb
@@ -3,13 +3,9 @@
 require "rails_helper"
 
 RSpec.describe "/unicode", type: :request do
-  include Dummy::Application.routes.url_helpers
-  include Capybara::DSL
-
   describe "#show" do
     it "renders PDF for multiple languages" do
       get unicode_path, headers: {accept: "application/pdf"}
-
       expect(response).to have_http_status(:ok)
     end
   end
