bd82d4abcd968f8cdfc159c2ae6b82e9b5a1f67e
Brooke Kuhlmann <brooke@alchemists.io>
2021-08-07 17:47:55 -0600

    Added version release notes

--- lib/prawn_plus/identity.rb
+++ lib/prawn_plus/identity.rb
@@ -5,7 +5,7 @@ module PrawnPlus
   module Identity
     NAME = "prawn_plus"
     LABEL = "Prawn+"
-    VERSION = "11.0.2"
+    VERSION = "11.0.3"
     VERSION_LABEL = "#{LABEL} #{VERSION}"
   end
 end
