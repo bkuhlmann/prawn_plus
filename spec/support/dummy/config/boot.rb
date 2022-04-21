234c799c85182674ab7dd44735cc7fa8fb12e8b4
Brooke Kuhlmann <brooke@alchemists.io>
2022-04-21 16:43:23 -0600

    Updated to Caliber 0.7.0
    Necessary to pick up RuboCop 1.28.0 changes.

--- spec/support/dummy/config/boot.rb
+++ spec/support/dummy/config/boot.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
-ENV["BUNDLE_GEMFILE"] ||= File.expand_path "../../../../Gemfile", __dir__
-
-require "bundler/setup" if File.exist? ENV["BUNDLE_GEMFILE"]
+require "bundler/setup"
 $LOAD_PATH.prepend File.expand_path("../../../../lib", __dir__)
