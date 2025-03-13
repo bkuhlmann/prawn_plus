8699041957f9ab302dd72f6c4f3b946fdb4616b5
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-13 16:02:30 -0600

    Fixed RuboCop Capybara/AmbiguousClick issue

--- spec/requests/documents_spec.rb
+++ spec/requests/documents_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "/documents" do
 
   it "answers a PDF file" do
     visit documents_path
-    click_on "PDF"
+    click_link "PDF"
 
     expect(page.response_headers["Content-Type"]).to match(%r(application/pdf))
   end
